9b296f913dcc3566497b3bcd50b014be016f086d
Eduardo da Silva <eduardo@blackvoodoocr.com>
2024-04-29 22:07:27 -0600

    Mobile view update for sidebar.jsx

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../ui/Sidebar.jsx";
 const AdminPage = () => {
     return (
         <div className="flex">
-            <div className="border-stone-150 h-[640px]  w-[80px] border-r-[1px] bg-colorPrimary150 md:h-dvh md:w-[300px]">
+            <div className="border-stone-150 min-h-[640px]  w-[80px] border-r-[1px] bg-colorPrimary150 md:h-dvh md:w-[300px]">
                 <Sidebar />
             </div>
             <div className="h-dvh flex-1 flex-grow bg-colorPrimary100">
